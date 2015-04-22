43151db2f7a9619d6dc304db84c9af2afea1a69e
Stanislav Bichenko <exxizt@gmail.com>
2015-04-22 23:25:40 +0300

    Browserify compatibility. Fixes #12

--- index.js
+++ index.js
@@ -4,7 +4,13 @@
 
    var libPrefix = process.env.COVER ? './lib-cov' : './lib';
 
-   module.exports = {
+   module.exports = process.browser ? {
+      compare: require('./lib/compare'),
+      XMLSerializer: require('./lib/canonizer'),
+      revXPath: require('./lib/revxpath'),
+      GroupingReporter: require('./lib/reporters/groupingReporter.js')
+   } :
+   {
       compare: require(libPrefix + '/compare'),
       XMLSerializer: require(libPrefix + '/canonizer'),
       revXPath: require(libPrefix + '/revxpath'),
